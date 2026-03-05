aa98c58805ea5e11910a08a26685d4537f14b879
Isam Balghari <isamabdullah88@gmail.com>
2026-03-05 22:42:59 +0500

    update HPC.

--- hpcrun.sh
+++ hpcrun.sh
@@ -6,9 +6,9 @@
 #SBATCH --ntasks=24                    # Request all 24 cores on that node
 #SBATCH --time=05-00:00:00                # Max wall time (adjust based on total configs: e.g., 100 configs * 4 mins = ~7 hours)
 #SBATCH --partition=cpuonly            # (Change this if your cluster uses a different default partition)
-#SBATCH --output=RelaxedFM-%j.out       # Standard output log (%j will be replaced by the unique Job ID)
-#SBATCH --error=RelaxedFM-%j.err        # Standard error log
-#SBATCH --exclusive                    # CRITICAL: Prevents VS Code or other users from sharing this node
+#SBATCH --output=logs/Kpts-10x10-Biaxial-AFM-%j.out       # Standard output log (%j will be replaced by the unique Job ID)
+#SBATCH --error=logs/Kpts-10x10-Biaxial-AFM-%j.err        # Standard error log
+#SBATCH --exclusive
 
 # --- 1. Clean and Load System Environment ---
 echo "Starting job on $HOSTNAME"
@@ -24,10 +24,14 @@ conda activate qe-env
 # Force OpenMP threads to 1 to prevent the cache thrashing we fixed earlier
 export OMP_NUM_THREADS=1
 
+echo "Installing libraries..."
+pip install -r requirements.txt
+
 echo "Beginning dataset generation..."
 
+
 # Run your Python script
-# (Ensure your Python script is set to loop sequentially and uses: mpirun -np 24 pw.x -npool 4)
-python run.py --WKDIR "/scratch/isam.Balghari/DataSets/Relaxed" --DBPATH "/scratch/isam.Balghari/DataSets/Relaxed/Relaxed.db" --N_CALCULATIONS 21 --CORES_PER_JOB 24 --PRERELAXED_DIR None
+# (Ensure your Python script is set to loop sequentially and uses: mpirun -np 24 pw.x -npool 8)
+python -u run.py --WKDIR "/scratch/isam.Balghari/DataSets/Kpts-10x10-Biaxial" --DBPATH "/scratch/isam.Balghari/DataSets/Kpts-10x10-Biaxial/AFM/Kpts-10x10-Biaxial-AFM.db" --N_CALCULATIONS 21 --CORES_PER_JOB 24 --PRERELAXED_DIR /scratch/isam.Balghari/DataSets/Relaxed-Biaxial
 
 echo "All configurations complete!"
